1d2f740d471fc75acf777b1ab92732acec2ec24f
kuroedov_maxim <Ni7pfpNh>
2018-05-24 01:47:41 +0300

    ....

--- core/views.py
+++ core/views.py
@@ -13,7 +13,8 @@ def index(request):
 
 @require_POST
 def create_table(request):
-    table_name = request.POST.get('name')
+    table_name = request.POST.get('table')
+    company_name = request.POST.get('name')
     scope = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive'
@@ -24,9 +25,23 @@ def create_table(request):
     try:
         credentials = ServiceAccountCredentials.from_json_keyfile_name(google_config, scope)
         gc = gspread.authorize(credentials)
-        sheet = gc.create(table_name)
-        sheet.share('vladzax95@gmail.com', perm_type='user', role='owner')
-        url = 'https://docs.google.com/spreadsheets/d/%s' % sheet.id
+        table = gc.create(table_name + ' ' + company_name)
+        table.share('mskuroedov@gmail.com', perm_type='user', role='owner')
+        sheet = table.sheet1
+        sheet.update_acell('A1', 'БЮДЖЕТ ДВИЖЕНИЯ ДЕНЕЖНЫХ СРЕДСТВ КОМПАНИИ ' + company_name)
+        sheet.update_acell('A3', 'Доходы')
+        sheet.update_acell('A4', 'Наименование проекта')
+        sheet.update_acell('A7', 'Итого Доходов')
+        sheet.update_acell('B7', '=SUM(B6:B4)')
+        sheet.update_acell('C7', '=SUM(C6:C4)')
+        sheet.update_acell('D7', '=SUM(D6:D4)')
+        sheet.update_acell('E7', '=SUM(E6:E4)')
+        sheet.update_acell('A9', 'Расходы')
+        sheet.update_acell('A10', '    Постоянные')
+        sheet.update_acell('A11', '    Прочие')
+        sheet.update_acell('A13', 'Итого расходы')
+        sheet.update_acell('A15', 'Итого Чистая прибыль')
+        url = 'https://docs.google.com/spreadsheets/d/%s' % table.id
     except Exception as e:
         error = str(e)
 
